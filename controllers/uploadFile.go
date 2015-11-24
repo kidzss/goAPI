22e6bf6be02c6cc389ec45ff669e112c7c054411
Ray <364664251@qq.com>
2015-11-23 22:52:08 -0800

    upload

--- controllers/uploadFile.go
+++ controllers/uploadFile.go
@@ -2,8 +2,12 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/Unknwon/com"
 	"github.com/astaxie/beego"
+	"os"
+	"path"
 	// "time"
+	"path/filepath"
 )
 
 type UploadContriller struct {
@@ -18,35 +22,61 @@ func (this *UploadContriller) Post() {
 		// 关闭文件
 		defer f.Close()
 	}
-	name := this.GetString("fileName", "")
+	name := this.GetString("userName", "")
 	if len(name) == 0 {
 		defer f.Close()
-		this.Data["json"] = map[string]interface{}{"success": 1, "message": "input file name"}
+		this.Data["json"] = map[string]interface{}{"errorCode": 1, "msg": "input file name", "result": nil}
 		this.ServeJson()
 		return
 
 	}
 	if err != nil {
 		// 获取错误则输出错误信息
-		this.Data["json"] = map[string]interface{}{"success": 1, "message": err.Error()}
+		this.Data["json"] = map[string]interface{}{"errorCode": 1, "msg": err.Error(), "result": nil}
 		this.ServeJson()
 		return
 	}
-	// 获取当前年月
-	//datePath := time.Now().Format("2006/01")
 	// 设置保存目录
-	dirPath := "./images" //+ datePath
+	dirPath := "./images/" + name + "/icon/"
 	// 设置保存文件名
-	FileName := name + h.Filename
-	// fmt.Println("time.Now():", time.Now())
+	FileName := dirPath + h.Filename
 	// 将文件保存到服务器中
-	err = this.SaveToFile("img", fmt.Sprintf("%s/%s", dirPath, FileName))
+	// 检查文件 and delete
+	getFilelist(dirPath)
+	if !com.IsExist(FileName) {
+		os.MkdirAll(path.Dir(FileName), os.ModePerm)
+	}
+
+	err = this.SaveToFile("img", FileName)
 	if err != nil {
 		// 出错则输出错误信息
-		this.Data["json"] = map[string]interface{}{"success": 1, "message": err.Error()}
+		this.Data["json"] = map[string]interface{}{"errorCode": 1, "msg": err.Error(), "result": nil}
 		this.ServeJson()
 		return
 	}
-	this.Data["json"] = map[string]interface{}{"success": 0, "message": "upload success", "imgUrl": "http://114.215.194.193:8088" + fmt.Sprintf("%s/%s", "/images", FileName)}
+	var url imageUrl
+	url.ImageUrl = "http://114.215.194.193:8088" + "/images/" + name + "/icon/" + h.Filename
+	this.Data["json"] = map[string]interface{}{"errorCode": 0, "msg": "upload success", "result": url}
 	this.ServeJson()
+}
+
+type imageUrl struct {
+	ImageUrl string
+}
+
+func getFilelist(path string) {
+	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		// println(path)
+		os.Remove(path)
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("filepath.Walk() returned %v\n", err)
+	}
 }
